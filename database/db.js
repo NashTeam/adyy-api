4d43150aa436ea0c13bceefba0023e537968254f
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-10-27 11:42:32 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,8 +2,8 @@ var __path = process.cwd(),
       monk = require('monk'),
      { color } = require(__path + '/lib/color.js')
 
-// Connection URL
-var url = 'adyy-api.vercel.app';
+// Connection URL biar rest api work:v
+var url = 'adyy-api.herokuapp.com';
 try {
 if(url == '') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
